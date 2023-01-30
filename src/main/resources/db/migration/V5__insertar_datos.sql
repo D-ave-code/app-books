57d6be5c0a48e4f02d99776d34d9d972bf9152a4
David Espinosa <espinozadavid34@yahoo.es>
2023-01-29 21:59:48 -0500

    s

---
+++ src/main/resources/db/migration/V5__insertar_datos.sql
@@ -0,0 +1,3 @@
+insert into books(isbn, title, author_id, price) values('11-11', 'title1', 1, 20);
+insert into books(isbn, title, author_id, price) values('22-22', 'title2', 2, 20);
+insert into books(isbn, title, author_id, price) values('33-33', 'title3', 2, 20);
